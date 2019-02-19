24d30673c94989dde2ba69b7e1b1ca850139bf06
bilnkat <billyjohn.general@gmail.com>
2019-02-19 15:54:47 -0800

    1.0  - changed database name from new_schma to jamf_database

--- tableLoader.py
+++ tableLoader.py
@@ -2,9 +2,9 @@ def loadTable(db, keys, headers, values):
 
     mydb = db
     mycursor = mydb.cursor()
-    mycursor.execute("DROP TABLE IF EXISTS new_schema.AllComputers;")
-    mycursor.execute("CREATE TABLE new_schema.AllComputers (" + headers + ");")
-    sql = "INSERT INTO new_schema.AllComputers (" + keys + ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    mycursor.execute("DROP TABLE IF EXISTS jamf_database.AllComputers;")
+    mycursor.execute("CREATE TABLE jamf_database.AllComputers (" + headers + ");")
+    sql = "INSERT INTO jamf_database.AllComputers (" + keys + ") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
     val = values
     mycursor.executemany(sql, val)
     mydb.commit()
